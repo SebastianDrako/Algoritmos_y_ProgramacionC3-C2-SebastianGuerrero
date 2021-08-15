5cf3ffd51b29348360229da08eac6b5b6fc4dfb6
Sebastian <sguerrero.sgc@gmail.com>
2021-08-15 16:29:01 -0500

    main

---
+++ Estructuras/1.py
@@ -0,0 +1,10 @@
+EdadA = input("Edad #1: ")
+EdadB = input("Edad #2: ")
+EdadC = input("Edad #3: ")
+
+EdadA = int(EdadA)
+EdadB = int(EdadB)
+EdadC = int(EdadC)
+
+
+print("El promedio entre ", EdadA, ", ", EdadB, " y ", EdadC, " es ", (( EdadA + (EdadB + EdadC) ) / 3))
---
+++ Estructuras/2.py
@@ -0,0 +1,3 @@
+inversion=input("Monto a invertir: ")
+inversion = int(inversion)
+print("En un mes usted ganara ", (inversion * 0.02), "para un total de", (inversion * 1.02))
