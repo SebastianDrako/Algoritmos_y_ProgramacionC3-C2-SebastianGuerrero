95dc67bf25f53c6a26350cb0d33a4c72f12b2f3e
Sebastian Guerrero Casadiegos <sguerrero.sgc@gmail.com>
2021-08-23 23:23:51 -0500

    main

--- Estructuras de Control Selectivas/1.py
+++ Estructuras de Control Selectivas/1.py
@@ -7,4 +7,4 @@ m = float(input("por cuentos meses lo invirtio: "))
 if (x * (i/100)) <= 100000:
     print("finalmente usted tendra en su cuenta", x + (x * (i/100))*m  )
 else:
-    print("finalmente usted tendra en su cuenta", x + pow((x * ((1+(i/100)))),m))
+    print("finalmente usted tendra en su cuenta", ((x * pow((1+(i/100)),m))+x))
